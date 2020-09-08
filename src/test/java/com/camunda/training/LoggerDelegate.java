9d3912cdb4eb9aea095848b95596ec538da8d09c
Thomas <thomas.heinrichs@camunda.com>
2020-09-08 12:50:29 +0200

    Mock Tweet Test and other JUnit enhancements

--- src/test/java/com/camunda/training/LoggerDelegate.java
+++ src/test/java/com/camunda/training/LoggerDelegate.java
@@ -1,42 +1,26 @@
 package com.camunda.training;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.Expression;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
 
-@Component
-public class TweetDelegate implements JavaDelegate {
+public class LoggerDelegate implements JavaDelegate {
 
     private final Logger LOGGER = LoggerFactory.getLogger(TweetDelegate.class.getName());
-    private final AccessToken ACCESS_TOKEN = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo", "B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
-    private Twitter twitter = new TwitterFactory().getInstance();
     private String message;
-
     public String getMessage() {
         return message;
     }
-
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    public Logger getLOGGER() {
-        return LOGGER;
     }
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         message = (String) delegateExecution.getVariable("Tweet");
         LOGGER.info("Publishing Tweet: "+ message);
-        twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
-        twitter.setOAuthAccessToken(ACCESS_TOKEN);
-        twitter.updateStatus(message);
         LOGGER.info("Tweet published successfully");
     }
 }
--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,34 +1,42 @@
 package com.camunda.training;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.mock.Mocks;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ProcessJUnitTest {
 
   @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+  public ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
 
   @Before
   public void setup() {
     init(rule.getProcessEngine());
+    Mocks.register("tweetDelegate", new LoggerDelegate());
   }
+
 
   @Test
   @Deployment(resources = "training.bpmn")
   public void testTweetDelegate(){
     //given
     Map<String, Object> variables = new HashMap<String, Object>();
-    variables.put("Tweet", "Make the world a better place! ");
+    variables.put("Tweet", "Make the world a better place!"+ UUID.randomUUID());
     variables.put("approval", true);
 
     //When
@@ -41,8 +49,28 @@ public class ProcessJUnitTest {
   }
 
   @Test
+  @Deployment(resources = "training.bpmn")
+  public void testTaskAssertion(){
+    // Given
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("Tweet", "Make Camunda Great Again!");
+    variables.put("approval", false);
+
+    // When
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("Process_TwitterQA", variables);
+    List<Task> taskList = taskService()
+            .createTaskQuery()
+            .taskCandidateGroup("management")
+            .list();
+
+    // Then
+    assertNotNull(taskList);
+    assertEquals(1, taskList.size() );
+  }
+
+  @Test
   @Deployment(resources="training.bpmn")
-  public void testHappyPath() {
+  public void testSadPath() {
     // Given
     Map<String, Object> variables = new HashMap<String, Object>();
     variables.put("Tweet", "Make Camunda Great Again!");
@@ -51,9 +79,11 @@ public class ProcessJUnitTest {
     // When
     ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("Process_TwitterQA", variables);
     Task task = taskService().createTaskQuery().singleResult();
+    assertEquals("Approve tweet", task.getName());
     taskService().complete(task.getId());
 
     // Then
+    assertEquals(0, runtimeService().createProcessInstanceQuery().count());
     assertThat(processInstance).isEnded();
   }
 }
